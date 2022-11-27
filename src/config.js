427f3c72b82b19fbc04f2454e3b5ed30f908f36e
AngelBL1611 <angelbl1611@gmail.com>
2022-11-27 16:54:43 -0600

    db connected to atlas

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
   SECRET: "meds-api",
+  DBPASSWORD: "lq2TjyNKoW9wY4SU",
 };
--- src/db.js
+++ src/db.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
+import config from "./config";
+const uri = `mongodb+srv://admin:${config.DBPASSWORD}@cluster0.a1zfhkd.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose
-  .connect("mongodb://localhost/medboard")
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((db) => console.log("db is connected"))
   .catch((error) => console.log(error));
